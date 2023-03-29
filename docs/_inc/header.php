fbcd858b97aa332f3b787c0a64ed22755df69948
Tristan White <oliver.tristan@gmail.com>
2023-03-29 10:15:58 +0200

    Add documentation structure

---
+++ docs/_inc/header.php
@@ -0,0 +1,11 @@
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--  Gridder CSS -->
+    <link rel="stylesheet" href="../dist/css/gridder.css">
+    <!--  Documentation CSS -->
+    <link rel="stylesheet" href="/docs/dist/css/styles.css">
+    <title>Gridder | Flexbox grid system</title>
+</head>
