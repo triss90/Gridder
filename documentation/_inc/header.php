d6c3a33a72c0e4212579efecada30a0eeeac79e4
Tristan White <oliver.tristan@gmail.com>
2023-03-30 16:29:27 +0200

    Add favicon

--- documentation/_inc/header.php
+++ documentation/_inc/header.php
@@ -12,4 +12,14 @@
     <link rel="stylesheet" href="/../dist/css/gridder.css">
     <!--  Documentation CSS -->
     <link rel="stylesheet" href="/documentation/dist/css/styles.css">
+    <!--Favicon-->
+    <link rel="apple-touch-icon" sizes="180x180" href="/documentation/dist/images/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/documentation/dist/images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/documentation/dist/images/favicon-16x16.png">
+    <link rel="manifest" href="/documentation/dist/images/site.webmanifest">
+    <link rel="mask-icon" href="/documentation/dist/images/safari-pinned-tab.svg" color="#1e64ff">
+    <link rel="shortcut icon" href="/documentation/dist/images/favicon.ico">
+    <meta name="msapplication-TileColor" content="#ff0000">
+    <meta name="msapplication-config" content="/documentation/dist/images/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
 </head>
