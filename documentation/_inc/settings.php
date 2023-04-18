8acd1b1e5da0d223304131bef6748cd2d3f22a42
Tristan White <oliver.tristan@gmail.com>
2023-04-18 13:08:37 +0200

    Update version

--- documentation/_inc/settings.php
+++ documentation/_inc/settings.php
@@ -1,5 +1,5 @@
 <?php
-$latestBuild = "v2.1.0";
+$latestBuild = "v2.2.0";
 $title = "Gridder | Flexbox Grid System";
 $description = "Gridder's fluid grid and breakpoints enable endless combinations of column sizes, offsets, allignments and viewport widths.";
 $keywords = "grid, grid system, flexbox grid, gridder, grid system, responsive grid, responsive, grid framework, responsive framework";
